9b4d60b46d75d1048492344caf59eacb9b4503ff
Maximilian Ferle <maximilian.ferle@singular-it.de>
2023-05-04 11:01:34 +0200

    minor changes

--- cluster.py
+++ cluster.py
@@ -1,12 +1,13 @@
 import pandas as pd
-import sklearn
+from sklearn.cluster import KMeans
 import numpy
 
-
-iris_data_train = pd.read_csv(".\\iris_data_train.csv", sep=";", index_col=0)
+iris_data_train = pd.read_csv(".\\iris_data_train.csv", sep=";", index_col=0).to_numpy()
 iris_labels_train = pd.read_csv(".\\iris_labels_train.csv", sep=";", index_col=0).to_numpy().flatten()
 
-iris_data_test = pd.read_csv(".\\iris_data_test.csv", sep=";", index_col=0)
+iris_data_test = pd.read_csv(".\\iris_data_test.csv", sep=";", index_col=0).to_numpy()
+
+print(iris_data_train[:, :])
 
 
 def some_function():
--- plot.py
+++ plot.py
@@ -6,13 +6,16 @@ iris_data_train = pd.read_csv(".\\iris_data_train.csv", sep=";", index_col=0)
 iris_labels_train = pd.read_csv(".\\iris_labels_train.csv", sep=";", index_col=0).to_numpy().flatten()
 iris_data_test = pd.read_csv(".\\iris_data_test.csv", sep=";", index_col=0)
 
-x = iris_data_train["sepal length (cm)"].to_numpy()
-y = iris_data_train["sepal width (cm)"].to_numpy()
+feat_1 = "petal length (cm)"
+feat_2 = "petal width (cm)"
+
+x = iris_data_train[feat_1].to_numpy()
+y = iris_data_train[feat_2].to_numpy()
 
 for label in np.unique(iris_labels_train):
     plt.scatter(x[iris_labels_train == label], y[iris_labels_train == label], label=label)
 
-plt.scatter(iris_data_test["sepal length (cm)"], iris_data_test["sepal width (cm)"], c="black", label="?")
+plt.scatter(iris_data_test[feat_1], iris_data_test[feat_2], c="black", label="?")
 
 plt.xlabel("sepal length (cm)")
 plt.ylabel("sepal width (cm)")
