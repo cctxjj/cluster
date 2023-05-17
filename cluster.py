fc3575f732df3dd11b88fc5910b375741addbc19
cctxjj <sebastian.dietrich7@outlook.de>
2023-05-17 19:44:20 +0200

    added more accurate predicting method

--- cluster.py
+++ cluster.py
@@ -1,11 +1,10 @@
 import pandas as pd
 from sklearn.cluster import KMeans
-import numpy
 
 iris_data_train = pd.read_csv("./iris_data_train.csv", sep=";", index_col=0).to_numpy()
 iris_labels_train = pd.read_csv("./iris_labels_train.csv", sep=";", index_col=0).to_numpy().flatten()
 
-iris_data_test = pd.read_csv("./iris_data_test.csv", sep=";").to_numpy()
+iris_data_test = pd.read_csv("./iris_data_test.csv", sep=";", index_col=0).to_numpy()
 iris_labels_test = pd.read_csv("./iris_labels_test.csv", sep=";").to_numpy()
 
 
@@ -28,25 +27,35 @@ class Predictor:
         self.cluster.max_iter = iterations
         self.cluster.n_iter_ = 10000
 
-        self.inaccuracy = self.__train__()
+        self.accuracy = self.__train__()
 
     def __train__(self):
         self.cluster.fit(self.traindata)
         prediction = self.cluster.predict(self.testdata)
 
-        expected = []
+        expected = {}
+        for i in range(0, self.cluster.n_clusters):
+            expected[i] = 0
         for item in self.test_result_data:
-            expected.append(item[1])
+            expected[item[1]] += 1
 
         var = 0
         for i in range(0, 3):
             for x in range(0, 3):
-                if [*prediction.flatten()].count(i) != expected.count(x):
-                    if x == 2:
-                        var += 1
+                if [*prediction].count(i) == expected[x]:
+                    var += 1
+                    break
 
-        return var / len(prediction)
-
+        return var / self.cluster.n_clusters * 100
 
     def predict(self, data):
         return self.cluster.predict(data)
+
+
+def predictionTest(ind):
+    predictor = Predictor()
+    print(predictor.accuracy)
+    for index, el in enumerate(iris_data_test):
+        if iris_labels_test[index][1] == ind:
+            print(predictor.predict([el]))
+
